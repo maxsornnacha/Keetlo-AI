1a4e34f10655aa952ddedf11e4e6d5a60516f335
Sornnacha Buranapongwattana <sornnacha.bu@gmail.com>
2025-10-14 17:55:41 +0700

    update

--- src/main/java/com/keetlo/ai/config/WebSocketConfig.java
+++ src/main/java/com/keetlo/ai/config/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins(CLIENT_URL)
+                .setAllowedOriginPatterns(CLIENT_URL)
                 .withSockJS();
     }
 
