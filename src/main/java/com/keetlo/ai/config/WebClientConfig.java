854e344e4051b54fc9ef38b8333a5579a5786967
Sornnacha Buranapongwattana <sornnacha.bu@gmail.com>
2025-10-14 14:10:43 +0700

    update

--- src/main/java/com/keetlo/ai/config/WebClientConfig.java
+++ src/main/java/com/keetlo/ai/config/WebClientConfig.java
@@ -6,7 +6,6 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import java.time.Duration;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/keetlo/ai/middleware/CorsConfig.java
+++ src/main/java/com/keetlo/ai/middleware/CorsConfig.java
@@ -1,16 +1,19 @@
 package com.keetlo.ai.middleware;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${client.url}") 
+    String clientUrl;
 
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3015")
+                .allowedOrigins(clientUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
