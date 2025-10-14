bb93f8a7dceabfdca424a9b1fb81d5df74948ac8
Sornnacha Buranapongwattana <sornnacha.bu@gmail.com>
2025-10-14 17:04:27 +0700

    update

--- src/main/java/com/keetlo/ai/middleware/CorsConfig.java
+++ src/main/java/com/keetlo/ai/middleware/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins(clientUrl)
+                .allowedOriginPatterns(clientUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
